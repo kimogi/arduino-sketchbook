2196d04d1606757051b982c4d8ef39c1cecfded0
kimogi <kimogi@teknique.com>
2015-08-02 19:10:43 +1200

    serial control added to heater coordinator

--- heater_coordinator/heater_coordinator/heater_coordinator.ino
+++ heater_coordinator/heater_coordinator/heater_coordinator.ino
@@ -1,13 +1,10 @@
 #include <SoftwareSerial.h>
 
-#define SWING_CMD "swg"
-#define POWER_CMD "pwd"
-#define WATT_CMD "wtt"
-
 SoftwareSerial zSerial(2, 3);
 
 String serialData = "";
 String address = "";
+String serialCmd = "";
 
 void send(String address, String cmd)
 {
@@ -21,7 +18,7 @@ void setup()
 {
   Serial.begin(9600);
   zSerial.begin(9600);
-  Serial.println("V13");
+  Serial.println("V14");
 }
 
 void loop()
@@ -45,22 +42,24 @@ void loop()
   {
     Serial.println(serialData);
     int indexOfZBC = serialData.indexOf("ZBC");
-    delay(2000);
     if (indexOfZBC > 0)
     {      
       address = serialData.substring(indexOfZBC + 7, indexOfZBC + 11);
-      delay(500);
-      send(address, POWER_CMD);
-      delay(1000);
-      send(address, WATT_CMD);
-      delay(1000);
-      send(address, WATT_CMD);
-      delay(1000);
-      send(address, SWING_CMD);
-      delay(3000);
-      send(address, POWER_CMD);
     }
-    Serial.println("-------------------------");
-    delay(500);
-  }   
+  }
+
+  while(Serial.available() > 0)
+  {
+    serialCmd += char(Serial.read());
+    delay(2);
+  }
+
+  if (address.length() > 0)
+  {
+    send(address, serialCmd);
+    delay(1000);
+    serialCmd = "";
+  }
+  Serial.println("-------------------------");
+  delay(500);
 }
